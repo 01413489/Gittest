903ac228e2e84483c96e93643bc57545a5a77f20
和昭 小磯 <llallapalooza@gmail.com>
2024-02-24 16:23:07 +0900

    足し算と引き算の機能を追加

--- GitSample/FrmMain.Designer.cs
+++ GitSample/FrmMain.Designer.cs
@@ -34,6 +34,8 @@
             this.txtExpression1 = new System.Windows.Forms.TextBox();
             this.txtExpression2 = new System.Windows.Forms.TextBox();
             this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
             label3 = new System.Windows.Forms.Label();
@@ -48,6 +50,26 @@
             label1.Size = new System.Drawing.Size(47, 22);
             label1.TabIndex = 0;
             label1.Text = "値１";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            label2.Location = new System.Drawing.Point(201, 9);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(47, 22);
+            label2.TabIndex = 2;
+            label2.Text = "値２";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            label3.Location = new System.Drawing.Point(12, 152);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(54, 22);
+            label3.TabIndex = 4;
+            label3.Text = "結果";
             // 
             // txtExpression1
             // 
@@ -65,16 +87,6 @@
             this.txtExpression2.Size = new System.Drawing.Size(162, 29);
             this.txtExpression2.TabIndex = 3;
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            label2.Location = new System.Drawing.Point(201, 9);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(47, 22);
-            label2.TabIndex = 2;
-            label2.Text = "値２";
-            // 
             // txtResult
             // 
             this.txtResult.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
@@ -83,21 +95,35 @@
             this.txtResult.Size = new System.Drawing.Size(351, 29);
             this.txtResult.TabIndex = 5;
             // 
-            // label3
+            // btnPlus
             // 
-            label3.AutoSize = true;
-            label3.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            label3.Location = new System.Drawing.Point(12, 152);
-            label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(54, 22);
-            label3.TabIndex = 4;
-            label3.Text = "結果";
+            this.btnPlus.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnPlus.Location = new System.Drawing.Point(16, 87);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(43, 36);
+            this.btnPlus.TabIndex = 6;
+            this.btnPlus.Text = "＋";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnMinus.Location = new System.Drawing.Point(65, 87);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(43, 36);
+            this.btnMinus.TabIndex = 6;
+            this.btnMinus.Text = "ー";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 241);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
             this.Controls.Add(this.txtResult);
             this.Controls.Add(label3);
             this.Controls.Add(this.txtExpression2);
@@ -119,6 +145,8 @@
         private System.Windows.Forms.TextBox txtExpression1;
         private System.Windows.Forms.TextBox txtExpression2;
         private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
     }
 }
 
