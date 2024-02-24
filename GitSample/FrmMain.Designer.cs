af155b02b721aefa4f5f85aca4223fdc9b0b4f18
01413489 <fujihara.minami@brave-heart.co.jp>
2024-02-24 17:34:36 +0900

    割り算を追加しました。

--- GitSample/FrmMain.Designer.cs
+++ GitSample/FrmMain.Designer.cs
@@ -37,6 +37,7 @@
             this.btnPlus = new System.Windows.Forms.Button();
             this.btnMinus = new System.Windows.Forms.Button();
             this.btnMultiple = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
             label3 = new System.Windows.Forms.Label();
@@ -46,9 +47,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            label1.Location = new System.Drawing.Point(12, 9);
+            label1.Location = new System.Drawing.Point(20, 14);
+            label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(47, 22);
+            label1.Size = new System.Drawing.Size(69, 33);
             label1.TabIndex = 0;
             label1.Text = "値１";
             // 
@@ -56,9 +58,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            label2.Location = new System.Drawing.Point(201, 9);
+            label2.Location = new System.Drawing.Point(335, 14);
+            label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(47, 22);
+            label2.Size = new System.Drawing.Size(69, 33);
             label2.TabIndex = 2;
             label2.Text = "値２";
             // 
@@ -66,42 +69,47 @@
             // 
             label3.AutoSize = true;
             label3.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            label3.Location = new System.Drawing.Point(12, 152);
+            label3.Location = new System.Drawing.Point(20, 228);
+            label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(54, 22);
+            label3.Size = new System.Drawing.Size(79, 33);
             label3.TabIndex = 4;
             label3.Text = "結果";
             // 
             // txtExpression1
             // 
             this.txtExpression1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txtExpression1.Location = new System.Drawing.Point(16, 43);
+            this.txtExpression1.Location = new System.Drawing.Point(27, 64);
+            this.txtExpression1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.txtExpression1.Name = "txtExpression1";
-            this.txtExpression1.Size = new System.Drawing.Size(162, 29);
+            this.txtExpression1.Size = new System.Drawing.Size(267, 39);
             this.txtExpression1.TabIndex = 1;
             // 
             // txtExpression2
             // 
             this.txtExpression2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txtExpression2.Location = new System.Drawing.Point(205, 43);
+            this.txtExpression2.Location = new System.Drawing.Point(342, 64);
+            this.txtExpression2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.txtExpression2.Name = "txtExpression2";
-            this.txtExpression2.Size = new System.Drawing.Size(162, 29);
+            this.txtExpression2.Size = new System.Drawing.Size(267, 39);
             this.txtExpression2.TabIndex = 3;
             // 
             // txtResult
             // 
             this.txtResult.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txtResult.Location = new System.Drawing.Point(16, 186);
+            this.txtResult.Location = new System.Drawing.Point(27, 279);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.txtResult.Name = "txtResult";
-            this.txtResult.Size = new System.Drawing.Size(351, 29);
+            this.txtResult.Size = new System.Drawing.Size(582, 39);
             this.txtResult.TabIndex = 5;
             // 
             // btnPlus
             // 
             this.btnPlus.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnPlus.Location = new System.Drawing.Point(16, 87);
+            this.btnPlus.Location = new System.Drawing.Point(27, 130);
+            this.btnPlus.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnPlus.Name = "btnPlus";
-            this.btnPlus.Size = new System.Drawing.Size(43, 36);
+            this.btnPlus.Size = new System.Drawing.Size(72, 54);
             this.btnPlus.TabIndex = 6;
             this.btnPlus.Text = "＋";
             this.btnPlus.UseVisualStyleBackColor = true;
@@ -110,9 +118,10 @@
             // btnMinus
             // 
             this.btnMinus.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnMinus.Location = new System.Drawing.Point(65, 87);
+            this.btnMinus.Location = new System.Drawing.Point(108, 130);
+            this.btnMinus.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnMinus.Name = "btnMinus";
-            this.btnMinus.Size = new System.Drawing.Size(43, 36);
+            this.btnMinus.Size = new System.Drawing.Size(72, 54);
             this.btnMinus.TabIndex = 6;
             this.btnMinus.Text = "－";
             this.btnMinus.UseVisualStyleBackColor = true;
@@ -121,19 +130,33 @@
             // btnMultiple
             // 
             this.btnMultiple.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnMultiple.Location = new System.Drawing.Point(114, 87);
+            this.btnMultiple.Location = new System.Drawing.Point(190, 130);
+            this.btnMultiple.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnMultiple.Name = "btnMultiple";
-            this.btnMultiple.Size = new System.Drawing.Size(43, 36);
+            this.btnMultiple.Size = new System.Drawing.Size(72, 54);
             this.btnMultiple.TabIndex = 7;
             this.btnMultiple.Text = "×";
             this.btnMultiple.UseVisualStyleBackColor = true;
             this.btnMultiple.Click += new System.EventHandler(this.btnMultiple_Click);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(279, 117);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 81);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "÷";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 241);
+            this.ClientSize = new System.Drawing.Size(640, 362);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnMultiple);
             this.Controls.Add(this.btnMinus);
             this.Controls.Add(this.btnPlus);
@@ -144,6 +167,7 @@
             this.Controls.Add(this.txtExpression1);
             this.Controls.Add(label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmMain";
@@ -161,6 +185,7 @@
         private System.Windows.Forms.Button btnPlus;
         private System.Windows.Forms.Button btnMinus;
         private System.Windows.Forms.Button btnMultiple;
+        private System.Windows.Forms.Button button1;
     }
 }
 
